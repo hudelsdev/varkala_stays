9402b849623b335f509c493f3cb7e642d643cd1e
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-07-02 12:14:19 +0530

    Update views.py

--- royal/views.py
+++ royal/views.py
@@ -2,7 +2,6 @@ import logging
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from django.http import HttpResponse
-from django.conf import settings
 
 
 logger = logging.getLogger(__name__)

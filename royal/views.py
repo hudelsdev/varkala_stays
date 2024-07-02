ea410e597fdc468e0dde95d4cddf2ab9ee0a28c2
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-07-02 12:07:59 +0530

    Update views.py

--- royal/views.py
+++ royal/views.py
@@ -51,7 +51,7 @@ def send_enquiry_email(request):
                     f'Check-in date: {check_in}\nCheck-out date: {check_out}\n'
                     f'Name: {name}\nContact number: {contact_number}\nNumber of people: {num_people}',
                     None,  
-                    ['sales@hudels.com'],
+                    ['varkalastays@gmail.com'],
                     fail_silently=False,
                 )
                 print("Email sent successfully")
